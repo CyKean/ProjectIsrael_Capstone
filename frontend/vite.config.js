33c9eb452280cd595250d38bb7ac7fae68990c03
CyKean <perjescykean356@gmail.com>
2025-08-01 02:46:15 +0800

    pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,24 +9,41 @@ export default defineConfig({
       registerType: 'autoUpdate', // Auto-update service worker
       includeAssets: ['favicon.ico', 'apple-touch-icon.png'], // Cached assets
       manifest: {
-        name: 'My Vue PWA',
-        short_name: 'VuePWA',
-        description: 'My Awesome Vue.js PWA',
+        name: 'PROJECT ISRAEL',
+        short_name: 'PJIR',
+        description: 'Project Israel',
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'public/images/logo/logo.png', // Place these in /public
+            src: 'maskable_icon_x192.png', // Place these in /public
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'any',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'maskable_icon_x512.png', // Use the same logo for 512x512
             sizes: '512x512',
             type: 'image/png',
+            purpose: 'any',
           },
         ],
+        screenshot: [
+          {
+            src: 'screenshot-desktop.png',
+            sizes: '1920x1080',
+            type: 'image/png',
+            form_factor: 'wide',
+          },
+          {
+            src: 'screenshot-mobile.png',
+            sizes: '720x1280',
+            type: 'image/png',
+            form_factor: 'narrow',
+          }
+        ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 50000000,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'], // Files to cache
       },
     }),
